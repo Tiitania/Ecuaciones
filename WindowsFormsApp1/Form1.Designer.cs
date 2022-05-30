10e50849e24ef0205d4713d01acc2fbc01967259
Octavio <Octavio@OCTAVIO-PC>
2022-05-30 14:47:06 -0500

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.datosBox = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chartA = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ecuacionBox = new System.Windows.Forms.TextBox();
+            this.button_Calcular = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(44, 119);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(831, 409);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Gainsboro;
+            this.tabPage1.Controls.Add(this.datosBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(823, 383);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Datos";
+            // 
+            // datosBox
+            // 
+            this.datosBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datosBox.Location = new System.Drawing.Point(79, 19);
+            this.datosBox.Name = "datosBox";
+            this.datosBox.Size = new System.Drawing.Size(256, 334);
+            this.datosBox.TabIndex = 0;
+            this.datosBox.Text = "";
+            this.datosBox.TextChanged += new System.EventHandler(this.datosBox_TextChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.chartA);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(823, 383);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Gráfica";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // chartA
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chartA.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chartA.Legends.Add(legend3);
+            this.chartA.Location = new System.Drawing.Point(25, 6);
+            this.chartA.Name = "chartA";
+            this.chartA.Size = new System.Drawing.Size(774, 381);
+            this.chartA.TabIndex = 0;
+            this.chartA.Text = "chart1";
+            this.chartA.Click += new System.EventHandler(this.chartA_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Formula    Y=";
+            // 
+            // ecuacionBox
+            // 
+            this.ecuacionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ecuacionBox.Location = new System.Drawing.Point(174, 53);
+            this.ecuacionBox.Name = "ecuacionBox";
+            this.ecuacionBox.Size = new System.Drawing.Size(209, 29);
+            this.ecuacionBox.TabIndex = 2;
+            // 
+            // button_Calcular
+            // 
+            this.button_Calcular.Location = new System.Drawing.Point(562, 56);
+            this.button_Calcular.Name = "button_Calcular";
+            this.button_Calcular.Size = new System.Drawing.Size(75, 23);
+            this.button_Calcular.TabIndex = 3;
+            this.button_Calcular.Text = "Calcular";
+            this.button_Calcular.UseVisualStyleBackColor = true;
+            this.button_Calcular.Click += new System.EventHandler(this.button_Calcular_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(389, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "procesar ecuacion";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(796, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 556);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button_Calcular);
+            this.Controls.Add(this.ecuacionBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartA)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox datosBox;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ecuacionBox;
+        private System.Windows.Forms.Button button_Calcular;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button_Calcular_Click(object sender, EventArgs e)
+        {
+            datosBox.Text = "";
+
+
+            this.chartA.Series.Clear();
+            var SeriesAY = chartA.Series.Add("ValorY");
+            SeriesAY.ChartType = SeriesChartType.Spline;
+            SeriesAY.XValueType = ChartValueType.Single;
+            chartA.Series["ValorY"].Color = Color.Red;
+
+
+            string ecua = ecuacionBox.Text;
+            string[] tokens = ecua.Split('^');
+            double xini = -10.0;
+            double y;
+
+
+            for(int i = 1; i<=20; i++)
+            {
+                y = System.Math.Pow(xini, Double.Parse(tokens[1]));
+                datosBox.Text = datosBox.Text + "    " + xini.ToString() + "    " + y.ToString() + Environment.NewLine;
+                xini += 1.0;
+                this.chartA.Series["ValorY"].Points.AddXY(xini, y);
+            }
+        }
+
+
+
+
+
+
+
+
+
+        private void datosBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chartA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
